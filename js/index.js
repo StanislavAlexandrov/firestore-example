239dcccecea13d35ced0758f4badd53ab442de89
Stanislav Alexandrov <stanislavalexandrov@gmail.com>
2020-10-07 01:41:47 +0500

    feature/added-words

--- js/index.js
+++ js/index.js
@@ -8,34 +8,114 @@ var db = firebase.firestore();
 
 var cityRef = db.collection('users').doc('testv');
 
+var usersDB = db.collection('users');
+
 var valueEl = document.getElementById('value');
-let storedNumber = 2;
+let storedNumber;
+let someOtherStoredNumber;
+let count = 0;
+const wordEvent = document.body.addEventListener('click', clickHandler);
 
-db.collection('users')
-    .doc('testv')
-    .onSnapshot(function (doc) {
-        console.log('Current data: ', doc.data());
-        storedNumber = doc.data().value;
-        valueEl.innerHTML = storedNumber;
-    });
-
-/* function getValues(collectionName, docName) {
-    return db
-        .collection(collectionName)
-        .doc(docName)
-        .get()
-        .then(function (doc) {
-            if (doc.exists) {
-                valueEl.innerHTML = doc.data().value;
-            }
-            return Promise.reject('No such document');
+document
+    .getElementById('incrementButton')
+    .addEventListener('click', function () {
+        someOtherStoredNumber++;
+        valueEl.innerHTML = someOtherStoredNumber;
+        cityRef.set({
+            value: someOtherStoredNumber,
         });
-} */
-
-document.getElementById('increment').addEventListener('click', function () {
-    storedNumber++;
-    valueEl.innerHTML = storedNumber;
-    cityRef.set({
-        value: storedNumber,
     });
+
+document.getElementById('resetButton').addEventListener('click', function () {
+    sentenceArray = ['hiiii', 'how', 'are', 'you'];
+    sentenceShuffled = shuffle([...sentenceArray]);
+    storedNumber = 0;
+    count = 0;
+    someOtherStoredNumber = 1212121;
+    valueEl.innerHTML = someOtherStoredNumber;
+    cityRef.set({
+        value: someOtherStoredNumber,
+    });
+    const selectClickyWords = document.querySelector('.clickyWords');
+    while (selectClickyWords.firstChild) {
+        selectClickyWords.removeChild(selectClickyWords.firstChild);
+    }
+
+    sentenceShuffled.forEach(sentence => buildButton(sentence));
+    for (let step = 0; step < sentenceArray.length; step++) {
+        word = sentenceArray[step];
+        db.collection('users')
+            .doc(word.toString())
+            .set({ word: word, index: step });
+    }
 });
+
+buildButton = sentence => {
+    const selectClickyWords = document.querySelector('.clickyWords');
+    const newButton = document.createElement('button');
+    newButton.setAttribute('class', 'btn btn-secondary btn-lg');
+    usersDB.doc(sentence).onSnapshot(function (doc) {
+        storedNumber = doc.data().word;
+        // valueEl.innerHTML = storedNumber;
+        newButton.setAttribute('wordn', storedNumber);
+        newButton.textContent = storedNumber;
+    });
+
+    selectClickyWords.appendChild(newButton);
+};
+
+function shuffle(array) {
+    var currentIndex = array.length,
+        temporaryValue,
+        randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+}
+
+// sentenceArray = ['hiiii', 'how', 'are', 'you'];
+// sentenceShuffled = shuffle([...sentenceArray]);
+
+sentenceShuffled.forEach(sentence => buildButton(sentence));
+for (let step = 0; step < sentenceArray.length; step++) {
+    word = sentenceArray[step];
+    db.collection('users')
+        .doc(word.toString())
+        .set({ word: word, index: step });
+}
+
+function clickHandler(e) {
+    let theWord = e.target.attributes.wordn.value;
+    let selectClicky = document.querySelector('.clickyWords');
+
+    if (theWord == sentenceArray[count]) {
+        theWordString = theWord.toString();
+        count++;
+        let selectWord = document.querySelector('[wordn=' + theWord + ']');
+
+        usersDB
+            .doc(theWordString)
+            .delete()
+            .then(function () {
+                console.log('Document successfully deleted!');
+            })
+            .catch(function (error) {
+                console.error('Error removing document: ', error);
+            });
+
+        // storedNumber = doc.data().word;
+
+        selectClicky.removeChild(selectWord);
+    }
+}
