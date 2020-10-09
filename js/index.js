e3734a0f842e49cb18e54316c3ae128dd219a0e8
Stanislav Alexandrov <stanislavalexandrov@gmail.com>
2020-10-09 12:33:05 +0500

    feature/added-real-time-button

--- js/index.js
+++ js/index.js
@@ -10,6 +10,8 @@ var cityRef = db.collection('users').doc('testv');
 
 var usersDB = db.collection('users');
 
+var textsDB = db.collection('texts');
+
 var valueEl = document.getElementById('value');
 let storedNumber;
 let someOtherStoredNumber;
@@ -26,29 +28,9 @@ document
         });
     });
 
-document.getElementById('resetButton').addEventListener('click', function () {
-    sentenceArray = ['hiiii', 'how', 'are', 'you'];
-    sentenceShuffled = shuffle([...sentenceArray]);
-    storedNumber = 0;
-    count = 0;
-    someOtherStoredNumber = 1212121;
-    valueEl.innerHTML = someOtherStoredNumber;
-    cityRef.set({
-        value: someOtherStoredNumber,
-    });
-    const selectClickyWords = document.querySelector('.clickyWords');
-    while (selectClickyWords.firstChild) {
-        selectClickyWords.removeChild(selectClickyWords.firstChild);
-    }
-
-    sentenceShuffled.forEach(sentence => buildButton(sentence));
-    for (let step = 0; step < sentenceArray.length; step++) {
-        word = sentenceArray[step];
-        db.collection('users')
-            .doc(word.toString())
-            .set({ word: word, index: step });
-    }
-});
+document
+    .getElementById('resetButton')
+    .addEventListener('click', resetFunction());
 
 buildButton = sentence => {
     const selectClickyWords = document.querySelector('.clickyWords');
@@ -63,6 +45,60 @@ buildButton = sentence => {
 
     selectClickyWords.appendChild(newButton);
 };
+
+document.getElementById('textsButton').addEventListener('click', textsFunction);
+
+function textsFunction() {
+    db.collection('texts')
+        .get()
+        .then(querySnapshot => {
+            querySnapshot.forEach(doc => {
+                if (`${doc.data().shown}` == 'true') {
+                    console.log('hit');
+                    document.getElementById('textsButton').textContent = `${
+                        doc.data().word
+                    }`;
+                } else {
+                    document.getElementById('textsButton').textContent =
+                        'not true';
+                }
+
+                console.log(`${doc.id} => ${doc.data().shown}`);
+            });
+        });
+}
+
+db.collection('texts').onSnapshot(function (querySnapshot) {
+    querySnapshot.forEach(function (doc) {
+        document.getElementById('textsButton').textContent = doc.data().word;
+    });
+});
+
+function resetFunction() {
+    return function () {
+        sentenceArray = ['hiiii', 'how', 'are', 'you'];
+        sentenceShuffled = shuffle([...sentenceArray]);
+        storedNumber = 0;
+        count = 0;
+        someOtherStoredNumber = 1212121;
+        valueEl.innerHTML = someOtherStoredNumber;
+        cityRef.set({
+            value: someOtherStoredNumber,
+        });
+        const selectClickyWords = document.querySelector('.clickyWords');
+        while (selectClickyWords.firstChild) {
+            selectClickyWords.removeChild(selectClickyWords.firstChild);
+        }
+
+        sentenceShuffled.forEach(sentence => buildButton(sentence));
+        for (let step = 0; step < sentenceArray.length; step++) {
+            word = sentenceArray[step];
+            db.collection('users')
+                .doc(word.toString())
+                .set({ word: word, index: step });
+        }
+    };
+}
 
 function shuffle(array) {
     var currentIndex = array.length,
